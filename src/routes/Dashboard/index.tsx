3a5db4f5801ee411b434e9219a44fc63f7b81107
Strato Ivan Di Luccio <ivan.diluccio@pagopa.it>
2025-11-20 12:20:11 +0100

    [P4PU-1037] update dashboard receipts button visibility and improve Italian translations

--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -68,16 +68,18 @@ const Dashboard = () => {
           <Typography variant="h6" component="h2" marginInlineStart={1}>
             {t('app.dashboard.lastTransactions')}
           </Typography>
-          <Button
-            component={Link}
-            to={ArcRoutes.RECEIPTS}
-            sx={{
-              width: theme.spacing(10),
-              justifyContent: 'flex-start',
-              p: 0
-            }}>
-            {t('app.dashboard.seeAllTransactions')}
-          </Button>
+          {data?.content?.length && data.content.length > 0 ? (
+            <Button
+              component={Link}
+              to={ArcRoutes.RECEIPTS}
+              sx={{
+                width: theme.spacing(10),
+                justifyContent: 'flex-start',
+                p: 0
+              }}>
+              {t('app.dashboard.seeAllTransactions')}
+            </Button>
+          ) : null}
         </Stack>
       </Stack>
       <Box
