ad0bc7bc919906dff024b52508fce9e9147f87fd
Strato Ivan Di Luccio <ivan.diluccio@pagopa.it>
2025-12-02 14:47:39 +0100

    [P4PU-1052] simplify QueryClient initialization by removing unused caches

--- src/index.tsx
+++ src/index.tsx
@@ -2,26 +2,14 @@ import React from 'react';
 import '@preact/signals-react/auto';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StoreProvider } from 'store/GlobalStore';
 import Matomo from 'components/Matomo';
 import CookieBanner from 'components/CookieBanner';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
-
-const queryClient = new QueryClient({
-  queryCache: new QueryCache({
-    onError: (error) => {
-      console.error('Query error:', error);
-    }
-  }),
-  mutationCache: new MutationCache({
-    onError: (error) => {
-      console.error('Mutation error:', error);
-    }
-  })
-});
+const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
