96d679242eca9aa0caaeea085368e17cc8e21f1e
Strato Ivan Di Luccio <ivan.diluccio@pagopa.it>
2025-12-11 16:21:15 +0100

    [P4PU-1078] enhance PaymentOptionItem with installment details and improved layout

--- src/routes/DebtPositionDetail/components/PaymentOptionItem.tsx
+++ src/routes/DebtPositionDetail/components/PaymentOptionItem.tsx
@@ -1,33 +1,67 @@
 import Card from '@mui/material/Card';
-import { DebtorPaymentOptionOverviewDTO } from '../../../../generated/data-contracts';
+import { DebtorPaymentOptionOverviewDTO, PaymentOptionType } from '../../../../generated/data-contracts';
 import React from 'react';
-import { FormControlLabel, Radio, Stack, Typography } from '@mui/material';
+import { Chip, FormControlLabel, Radio, Stack, Typography } from '@mui/material';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem, { timelineItemClasses } from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineDot from '@mui/lab/TimelineDot/TimelineDot';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent/TimelineContent';
+
+const ExtraInfo = (props: { installments: DebtorPaymentOptionOverviewDTO['installments'] }) => {
+  return <Timeline       sx={{
+        [`& .${timelineItemClasses.root}:before`]: {
+          flex: 0,
+          padding: 0,
+        },
+      }}>
+    {props.installments.map((installment, index) => (
+      <TimelineItem key={installment.installmentId}>
+        <TimelineSeparator>
+          <TimelineDot />
+          { index < props.installments.length - 1 && <TimelineConnector /> }
+        </TimelineSeparator>
+        <TimelineContent>{`Rata ${index + 1} ${installment.amountCents} ${installment.status} entro il ${installment.dueDate}`} </TimelineContent>
+      </TimelineItem>
+    ))}
+  </Timeline>;
+}
 
 interface PaymentOptionProps extends DebtorPaymentOptionOverviewDTO {
   selectionStatus: 'selected' | 'unselected' | 'disabled';
 }
 
 const PaymentOption = (props: PaymentOptionProps) => {
+  const isSelected = props.selectionStatus === 'selected';
+  const paymentOptionType = props.paymentOptionType;
+
   return (
     <Card
       sx={{
         border: 2,
-        color: props.selectionStatus === 'selected' ? 'primary.main' : 'divider',
+        color: isSelected ? 'primary.main' : 'divider',
         padding: 3
       }}>
       <Stack direction="row" justifyContent="space-between">
-        <Stack direction="row">
+        <Stack>
           <FormControlLabel
             value={props.paymentOptionId}
             control={<Radio />}
             label={props.paymentOptionType}
           />
+          {
+            paymentOptionType === PaymentOptionType.INSTALLMENTS && <Chip label={`${props.installments.length} Rate`} />
+          }
         </Stack>
         <Stack>
           <Typography>{props.installments[0].amountCents}</Typography>
           <Typography>{props.installments[0].dueDate}</Typography>
         </Stack>
       </Stack>
+      { paymentOptionType === PaymentOptionType.INSTALLMENTS && isSelected ?
+        <ExtraInfo installments={props.installments} /> : null
+      }
     </Card>
   );
 };
