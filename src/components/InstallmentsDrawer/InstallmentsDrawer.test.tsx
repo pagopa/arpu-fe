d74137da59437876be65848991ce272b7427dee9
Strato Ivan Di Luccio <ivan.diluccio@pagopa.it>
2026-01-15 14:14:57 +0100

    [P4PU-1080] correct indentation in InstallmentsDrawer test data

--- src/components/InstallmentsDrawer/InstallmentsDrawer.test.tsx
+++ src/components/InstallmentsDrawer/InstallmentsDrawer.test.tsx
@@ -6,21 +6,21 @@ import { InstallmentStatus } from '../../../generated/data-contracts';
 import { setInstallmentsDrawerState } from 'store/installmentsDrawer';
 
 const items: InstallmentDrawerItem[] = [
-    {
-      installmentId: 1,
-      rateIndex: 1,
-      dueDate: '2024-08-31',
-      amountCents: 150,
-      paFullName: 'ENTE TEST',
-      paTaxCode: 'JHNDOE80A01F205X',
-      debtPositionId: 123,
-      paymentOptionId: 456,
-      iuv: 'IUV1234567890',
-      nav: 'NAV1234567890',
-      remittanceInformation: 'Description 1',
-      status: InstallmentStatus.EXPIRED
-    },
-    {
+  {
+    installmentId: 1,
+    rateIndex: 1,
+    dueDate: '2024-08-31',
+    amountCents: 150,
+    paFullName: 'ENTE TEST',
+    paTaxCode: 'JHNDOE80A01F205X',
+    debtPositionId: 123,
+    paymentOptionId: 456,
+    iuv: 'IUV1234567890',
+    nav: 'NAV1234567890',
+    remittanceInformation: 'Description 1',
+    status: InstallmentStatus.EXPIRED
+  },
+  {
     installmentId: 2,
     rateIndex: 2,
     dueDate: '2024-09-31',
@@ -34,7 +34,7 @@ const items: InstallmentDrawerItem[] = [
     remittanceInformation: 'Description 2',
     status: InstallmentStatus.PAID
   },
-    {
+  {
     installmentId: 3,
     rateIndex: 3,
     dueDate: '2024-10-31',
@@ -48,7 +48,7 @@ const items: InstallmentDrawerItem[] = [
     remittanceInformation: 'Description 3',
     status: InstallmentStatus.UNPAID
   },
-    {
+  {
     installmentId: 4,
     rateIndex: 4,
     dueDate: '2024-11-31',
@@ -61,7 +61,7 @@ const items: InstallmentDrawerItem[] = [
     nav: 'NAV1234567893',
     remittanceInformation: 'Description 4',
     status: InstallmentStatus.UNPAID
-  },
+  }
 ];
 
 describe('InatallmentsDrawer', () => {
