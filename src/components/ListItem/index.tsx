90f957ae0bf932847f34e9be4892fd527ff4f746
Michele Moio <michelemoiodev@gmail.com>
2026-02-11 14:23:35 +0100

    fix: [P4PU-1219] removes amount if missing (#80)

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -40,79 +40,81 @@ export const ListItem = ({
 
   return (
     <Card>
-      <Stack
-        p={{ sm: 3, xs: 2 }}
-        direction="row"
-        justifyContent={'space-between'}
-        alignItems="center">
-        <Stack direction="row" spacing={{ xs: 0, sm: 2 }} alignItems="center">
-          {smUp ? icon : null}
-          <Stack>
-            <Typography
-              variant="body2"
-              fontWeight={600}
-              sx={{
-                whiteSpace: 'nowrap',
-                overflow: 'hidden',
-                textOverflow: 'ellipsis',
-                width: '100%'
-              }}>
-              {title}
-            </Typography>
-            {subtitle && (
+      <Stack height={theme.spacing(19)} justifyContent="center">
+        <Stack
+          p={{ sm: 3, xs: 2 }}
+          direction="row"
+          justifyContent={'space-between'}
+          alignItems="center">
+          <Stack direction="row" spacing={{ xs: 0, sm: 2 }} alignItems="center">
+            {smUp ? icon : null}
+            <Stack>
               <Typography
                 variant="body2"
+                fontWeight={600}
                 sx={{
                   whiteSpace: 'nowrap',
                   overflow: 'hidden',
                   textOverflow: 'ellipsis',
                   width: '100%'
                 }}>
-                {subtitle}
+                {title}
               </Typography>
-            )}
-          </Stack>
-        </Stack>
-
-        <Stack
-          direction="row"
-          alignItems="center"
-          gap={2}
-          justifyContent="space-between"
-          width={{ xs: '50%', sm: '30%', md: '25%', xl: '20%' }}>
-          <Stack gap={2} alignItems="center" direction="row">
-            <Divider orientation="vertical" flexItem />
-            <Stack gap={1}>
-              {fields.map((field, index) => (
-                <Stack key={index}>
-                  <Typography variant="caption" color="text.secondary" fontSize={16}>
-                    {field.label}
-                  </Typography>
-                  <Typography
-                    variant={field.variant || 'body2'}
-                    fontWeight={field.fontWeight || 600}
-                    fontSize={18}
-                    sx={{
-                      whiteSpace: 'nowrap',
-                      overflow: 'hidden',
-                      textOverflow: 'ellipsis',
-                      width: '100%'
-                    }}>
-                    {field.value}
-                  </Typography>
-                </Stack>
-              ))}
+              {subtitle && (
+                <Typography
+                  variant="body2"
+                  sx={{
+                    whiteSpace: 'nowrap',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    width: '100%'
+                  }}>
+                  {subtitle}
+                </Typography>
+              )}
             </Stack>
           </Stack>
-          {sm && (
-            <IconButton
-              onClick={handleClick}
-              size="small"
-              aria-label={detailAriaLabel}
-              data-testid={detailTestId}>
-              <ChevronRight />
-            </IconButton>
-          )}
+
+          <Stack
+            direction="row"
+            alignItems="center"
+            gap={2}
+            justifyContent="space-between"
+            width={{ xs: '50%', sm: '30%', md: '25%', xl: '20%' }}>
+            <Stack gap={2} alignItems="center" direction="row">
+              <Divider orientation="vertical" flexItem sx={{ height: theme.spacing(13) }} />
+              <Stack gap={1}>
+                {fields.map((field, index) => (
+                  <Stack key={index}>
+                    <Typography variant="caption" color="text.secondary" fontSize={16}>
+                      {field.label}
+                    </Typography>
+                    <Typography
+                      variant={field.variant || 'body2'}
+                      fontWeight={field.fontWeight || 600}
+                      fontSize={18}
+                      sx={{
+                        whiteSpace: 'nowrap',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        width: '100%'
+                      }}>
+                      {field.value}
+                    </Typography>
+                  </Stack>
+                ))}
+              </Stack>
+            </Stack>
+            {sm && (
+              <IconButton
+                onClick={handleClick}
+                size="small"
+                aria-label={detailAriaLabel}
+                data-testid={detailTestId}>
+                <ChevronRight />
+              </IconButton>
+            )}
+          </Stack>
         </Stack>
       </Stack>
     </Card>
