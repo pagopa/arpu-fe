e43e90e3a3a565b95c93be9f52ab16776e2e17b4
Michele Moio <michelemoiodev@gmail.com>
2026-03-05 08:55:20 +0100

    feat: [P4PU-1263] language switcher

---
+++ src/translations/languages.ts
@@ -0,0 +1,19 @@
+import { Languages } from '@pagopa/mui-italia';
+
+export const languages: Languages = {
+  it: {
+    it: 'Italiano',
+    en: 'Inglese',
+    fr: 'Francese'
+  },
+  en: {
+    it: 'Italian',
+    en: 'English',
+    fr: 'French'
+  },
+  fr: {
+    it: 'Italien',
+    en: 'Anglais',
+    fr: 'Francais'
+  }
+};
