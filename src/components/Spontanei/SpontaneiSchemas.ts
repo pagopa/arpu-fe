b6d99ebcfa6dc308f58bc6d291addd14a6070727
Strato Ivan Di Luccio <ivan.diluccio@pagopa.it>
2026-03-09 14:09:37 +0100

    [P4PU-1269] Make email field required in DebtorSection and update its validation schema to enforce email validation.

--- src/components/Spontanei/SpontaneiSchemas.ts
+++ src/components/Spontanei/SpontaneiSchemas.ts
@@ -28,7 +28,7 @@ const getPaymentNoticeInfoSchema = (t: (key: string) => string) =>
         /(^[A-Za-z]{6}[0-9]{2}[A-Za-z]{1}[0-9]{2}[A-Za-z]{1}[0-9]{3}[A-Za-z]{1}$)|(^[0-9]{11}$)|ANONIMO/,
         t('spontanei.form.errors.fullName')
       ),
-    email: z.string().email(t('spontanei.form.errors.email')).optional().or(z.literal(''))
+    email: z.string().email(t('spontanei.form.errors.email'))
   });
 
 export default getPaymentNoticeInfoSchema;
