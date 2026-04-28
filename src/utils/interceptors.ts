745cbf9da8c4d5b1e8492411f9d106c1466fdf24
EmVal <147710433+EmanueleValentini1@users.noreply.github.com>
2026-04-28 16:00:45 +0200

    feat: [P4PU-1327] public interceptor (#172)

--- src/utils/interceptors.ts
+++ src/utils/interceptors.ts
@@ -35,6 +35,9 @@ export const setupInterceptors = (client: Client) => {
         if (isRecaptchaError(error)) {
           const toUrl = ROUTES.public.COURTESY_PAGE.replace(':outcome', 'verifica-non-riuscita');
           window.location.replace(toUrl);
+        } else if (utils.storage.user.isAnonymous()) {
+          const toUrl = ROUTES.public.COURTESY_PAGE.replace(':outcome', 'errore-server');
+          window.location.replace(toUrl);
         } else {
           const toUrl = ROUTES.public.COURTESY_PAGE.replace(':outcome', OUTCOMES['401']);
           utils.storage.user.logOut();
