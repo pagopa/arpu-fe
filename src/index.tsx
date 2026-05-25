1d23d8c7a10906f6a3f51edafc9331b1cccc54c5
Vito Falco <vitofalco@gmail.com>
2026-05-25 12:08:08 +0200

    fix: [P4PU-1406] [P4PU-14025] Add more space between children div in summary (spontanei). Drop refresh query on focus away (#192)

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,13 @@ import { StoreProvider } from 'store/GlobalStore';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+});
 
 root.render(
   <React.StrictMode>
