f8ca769ecbf0e3751808dbd99beaf4fc9766e551
EmanueleValentini1 <emanuele.valentini@nttdata.com>
2026-06-03 09:06:53 +0200

    hide collapse/expand toggle from screen readers

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -76,6 +76,8 @@ export const Sidebar: React.FC = () => {
                 <IconButton
                   data-testid="collapseClose"
                   aria-label={t(!collapsed ? 'sidebar.collapse' : 'sidebar.expand')}
+                  aria-hidden="true"
+                  tabIndex={-1}
                   onClick={() => changeMenuState()}
                   size="large">
                   <CloseIcon />
@@ -107,6 +109,8 @@ export const Sidebar: React.FC = () => {
                   sx={{ padding: { xs: 1, lg: 0 } }}
                   data-testid="hamburgerButton"
                   aria-label={t(!collapsed ? 'sidebar.collapse' : 'sidebar.expand')}
+                  aria-hidden="true"
+                  tabIndex={-1}
                   onClick={() => changeMenuState()}
                   size="large">
                   <MenuIcon color="action" />
