fc9e0ac649e3ceef41d243beceece43d5ca6e6a7
Michele Moio <michelemoiodev@gmail.com>
2026-06-24 11:39:47 +0200

    feat: [P4PU-1430] list items styling (#213)

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import { ChevronRight } from '@mui/icons-material';
-import {
-  Stack,
-  Typography,
-  IconButton,
-  Card,
-  Theme,
-  useMediaQuery,
-  Divider,
-  Box
-} from '@mui/material';
+import { Stack, Typography, IconButton, Theme, useMediaQuery, Divider, Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { theme } from '@pagopa/mui-italia';
 
@@ -40,7 +31,7 @@ export const ListItem = ({
   detailTestId = 'detail-button'
 }: GenericListItemProps) => {
   const sm = useMediaQuery((theme: Theme) => theme.breakpoints.up('sm'));
-  const smUp = useMediaQuery(theme.breakpoints.up('sm'));
+  const md = useMediaQuery(theme.breakpoints.up('md'));
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -48,96 +39,100 @@ export const ListItem = ({
   };
 
   return (
-    <Card role="listitem">
-      <Stack height={theme.spacing(19)} justifyContent="center">
-        <Stack
-          p={{ sm: 3, xs: 2 }}
-          direction="row"
-          justifyContent={'space-between'}
-          alignItems="center">
-          <Stack direction="row" spacing={{ xs: 0, sm: 2 }} alignItems="center">
-            {smUp ? (
-              <Box aria-hidden="true" sx={{ display: 'flex' }}>
-                {icon}
-              </Box>
-            ) : null}
-            <Stack>
-              <Typography
-                variant="body2"
-                component="h3"
-                fontWeight={600}
-                sx={{
-                  whiteSpace: 'nowrap',
-                  overflow: 'hidden',
-                  textOverflow: 'ellipsis',
-                  width: '100%'
-                }}>
-                {title}
-              </Typography>
-              {subtitle && (
+    <Stack
+      bgcolor="background.paper"
+      borderRadius={2}
+      gap={3}
+      role="listitem"
+      p={{ xs: 2, sm: 3 }}
+      direction={{ xs: 'column', md: 'row' }}
+      justifyContent={'space-between'}
+      alignItems={{ xs: 'flex-start', md: 'center' }}>
+      <Stack direction="row" spacing={{ xs: 0, sm: 2 }} alignItems="center">
+        {sm ? (
+          <Box aria-hidden="true" sx={{ display: 'flex' }}>
+            {icon}
+          </Box>
+        ) : null}
+        <Stack>
+          <Typography
+            variant="body2"
+            component="h3"
+            fontWeight={600}
+            sx={{
+              whiteSpace: 'nowrap',
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              width: '100%'
+            }}>
+            {title}
+          </Typography>
+          {subtitle && (
+            <Typography
+              variant="body2"
+              component="h4"
+              sx={{
+                whiteSpace: 'nowrap',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                width: '100%'
+              }}>
+              {subtitle}
+            </Typography>
+          )}
+        </Stack>
+      </Stack>
+
+      {md ? null : <Divider flexItem aria-hidden="true" />}
+      <Stack
+        minHeight={theme.spacing(10)}
+        direction="row"
+        alignItems="center"
+        gap={2}
+        justifyContent="space-between"
+        width={{ xs: '100%', md: '50%', lg: '25%' }}>
+        <Stack gap={2} alignItems="center" direction="row">
+          {md ? (
+            <Divider
+              orientation="vertical"
+              flexItem
+              aria-hidden="true"
+              sx={{ minHeight: theme.spacing(10) }}
+            />
+          ) : null}
+          <Stack component="dl" gap={1} sx={{ m: 0 }}>
+            {fields.map((field, index) => (
+              <Box key={index}>
+                <Typography component="dt" variant="caption" color="text.secondary" fontSize={16}>
+                  {field.label}
+                </Typography>
                 <Typography
-                  variant="body2"
-                  component="h4"
+                  component="dd"
+                  variant={field.variant || 'body2'}
+                  fontWeight={field.fontWeight || 600}
+                  fontSize={18}
                   sx={{
+                    m: 0,
                     whiteSpace: 'nowrap',
                     overflow: 'hidden',
                     textOverflow: 'ellipsis',
                     width: '100%'
                   }}>
-                  {subtitle}
+                  {field.value}
                 </Typography>
-              )}
-            </Stack>
-          </Stack>
-
-          <Stack
-            direction="row"
-            alignItems="center"
-            gap={2}
-            justifyContent="space-between"
-            width={{ xs: '50%', sm: '30%', md: '25%', xl: '20%' }}>
-            <Stack gap={2} alignItems="center" direction="row">
-              <Divider orientation="vertical" flexItem sx={{ height: theme.spacing(13) }} />
-              <Stack component="dl" gap={1} sx={{ m: 0 }}>
-                {fields.map((field, index) => (
-                  <Box key={index}>
-                    <Typography
-                      component="dt"
-                      variant="caption"
-                      color="text.secondary"
-                      fontSize={16}>
-                      {field.label}
-                    </Typography>
-                    <Typography
-                      component="dd"
-                      variant={field.variant || 'body2'}
-                      fontWeight={field.fontWeight || 600}
-                      fontSize={18}
-                      sx={{
-                        m: 0,
-                        whiteSpace: 'nowrap',
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis',
-                        width: '100%'
-                      }}>
-                      {field.value}
-                    </Typography>
-                  </Box>
-                ))}
-              </Stack>
-            </Stack>
-            {sm && (
-              <IconButton
-                onClick={handleClick}
-                size="small"
-                aria-label={detailAriaLabel}
-                data-testid={detailTestId}>
-                <ChevronRight />
-              </IconButton>
-            )}
+              </Box>
+            ))}
           </Stack>
         </Stack>
+        <IconButton
+          color="primary"
+          onClick={handleClick}
+          size="small"
+          aria-label={detailAriaLabel}
+          data-testid={detailTestId}>
+          <ChevronRight />
+        </IconButton>
       </Stack>
-    </Card>
+    </Stack>
   );
 };
