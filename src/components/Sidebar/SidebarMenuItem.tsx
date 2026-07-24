f70aa385547645cab63f551fcefdb59c6d07cabc
Strato Ivan Di Luccio <ivan.diluccio@pagopa.it>
2026-07-24 15:58:14 +0200

    [P4PU-1464] code linting

--- src/components/Sidebar/SidebarMenuItem.tsx
+++ src/components/Sidebar/SidebarMenuItem.tsx
@@ -26,9 +26,7 @@ export const SidebarMenuItem = ({ collapsed, item, onClick }: Props) => {
   const theme = useTheme();
 
   const MenuItem = (
-    <ListItem
-      disablePadding
-      data-testid={item.dataTestId}>
+    <ListItem disablePadding data-testid={item.dataTestId}>
       <ListItemButton
         end={item.end || false}
         component={NavLink}
